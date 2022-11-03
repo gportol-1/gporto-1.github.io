8de4259f299ff65b9fa6e1705dd9b06b61f66104
Gabriel Luiz Porto <117106239+gportol-1@users.noreply.github.com>
2022-11-03 19:12:56 -0300

    aula 4 nlw
    Signed-off-by: Gabriel Luiz Porto <117106239+gportol-1@users.noreply.github.com>

--- 3 - NLW-Copa-Rocketseat/js/main.js
+++ 3 - NLW-Copa-Rocketseat/js/main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = -0.3;
 function createCard(date, day ,games) {
+  delay = delay + 0.3;
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s ">
       <h2>${date} <span>${day}</span> </h2>
       <ul>
         ${games}
@@ -19,22 +21,29 @@ function createCard(date, day ,games) {
       `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo da NLW"/>
-  </header>
-  <main id="cards">
-    ${createCard("20/11", "domingo",
-      createGame("brazil","16:00","serbia")
-    )}
-    ${createCard("21/11", "segunda", 
-      createGame("brazil","16:00","serbia")
-    )}    
-    ${createCard("22/11", "terça", 
-      createGame("brazil","16:00","serbia")
-    )}    
-    ${createCard("23/11", "quarta", 
-      createGame("brazil","16:00","serbia")
-    )}      
-  </main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard("20/11", "domingo", createGame("catar", "13:00", "equador")) +
+  createCard(
+    "21/11",
+    "segunda",
+    createGame("inglaterra", "10:00", "irã") +
+      createGame("senegal", "13:00", "holanda") +
+      createGame("estados-unidos", "16:00", "gales")
+  ) +
+  createCard(
+    "22/11",
+    "terça",
+    createGame("argentina", "07:00", "arabia-saudita") +
+      createGame("dinamarca", "10:00", "tunisia") +
+      createGame("mexico", "13:00", "polonia") +
+      createGame("frança", "16:00", "australia")
+  ) +
+  createCard(
+    "23/11",
+    "quarta",
+    createGame("marrocos", "07:00", "croacia") +
+      createGame("alemanha", "10:00", "japao") +
+      createGame("espanha", "13:00", "costa-rica") +
+      createGame("belgica", "16:00", "canada")
+  )    
+
